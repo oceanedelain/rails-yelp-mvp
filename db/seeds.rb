8952ebb590804d48c61beead5e2c3ea83d214352
Oceane Delain <delain.oceane@gmail.com>
2021-05-12 16:50:48 +0200

    Yelp MVP exo 2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create ([
+  { name: 'Surpriz', address: 'Rue Oberkampf', phone_number: '01 41 51 61 71', category: 'french'},
+  { name: 'Alicheur', address: 'Rue saint-maur', phone_number: '06 36 51 61 71', category: 'chinese'},
+  { name: 'I love bobun', address: 'Rue des bobuns', phone_number: '02 51 10 38 45', category: 'chinese'},
+  { name: 'I love chicken', address: 'Rue des poulettes', phone_number: '06 11 40 58 15', category: 'belgian'},
+  { name: 'Pasta fever', address: 'Rue des mamma', phone_number: '06 45 10 58 15', category: 'italian'}
+])
